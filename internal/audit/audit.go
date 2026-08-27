cd2d28aeb35b8f6c3146a64d5acf4f95a9b3fe25
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-08-27 16:40:52 +0300

    feat: add Event DTO and Observer interface

---
+++ internal/audit/audit.go
@@ -0,0 +1,17 @@
+package audit
+
+import "time"
+
+type Event struct {
+	Timestamp int64    `json:"ts"`
+	Metrics   []string `json:"metrics"`
+	IPAddress string   `json:"ip_address"`
+}
+
+func NewEvent(metrics []string, addr string) Event {
+	return Event{Timestamp: time.Now().Unix(), Metrics: metrics, IPAddress: addr}
+}
+
+type Observer interface {
+	Notify(event Event) error
+}
