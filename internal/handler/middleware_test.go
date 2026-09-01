4ddd4563e93f66fde73f95c35888eceda0629322
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-09-01 11:38:54 +0300

    test: add benchmark for CompressMiddleware pool reuse

--- internal/handler/middleware_test.go
+++ internal/handler/middleware_test.go
@@ -9,6 +9,42 @@ import (
 	"github.com/AVZotov/metrics/internal/sign"
 )
 
+func BenchmarkCompressMiddleware(b *testing.B) {
+	benchmarks := []struct {
+		name string
+		data []byte
+	}{
+		{name: "100 bytes", data: bytes.Repeat([]byte("a"), 100)},
+		{name: "1024 bytes", data: bytes.Repeat([]byte("a"), 1024)},
+		{name: "10240 bytes", data: bytes.Repeat([]byte("a"), 10240)},
+	}
+	for _, bm := range benchmarks {
+		b.Run(
+			bm.name, func(b *testing.B) {
+				{
+					b.ReportAllocs()
+					next := http.HandlerFunc(
+						func(w http.ResponseWriter, r *http.Request) {
+							w.Header().Set("Content-Type", "application/json")
+							w.WriteHeader(http.StatusOK)
+							_, _ = w.Write(bm.data)
+						},
+					)
+					handler := CompressMiddleware()(next)
+					for b.Loop() {
+						b.StopTimer()
+						req := httptest.NewRequest(http.MethodGet, "/", nil)
+						req.Header.Set("Accept-Encoding", "gzip")
+						rec := httptest.NewRecorder()
+						b.StartTimer()
+						handler.ServeHTTP(rec, req)
+					}
+				}
+			},
+		)
+	}
+}
+
 func BenchmarkSignMiddleware(b *testing.B) {
 	const key = "super secret key"
 	benchmarks := []struct {
